8738fc53898877226cea964923aef60947b51938
Ivan Matuck <ivan.matuck@hotmail.com>
2020-02-27 20:44:43 -0300

    Ajustando arquivos

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -25,7 +26,7 @@ public class SessaoController {
 
 	@Autowired
 	private FilmeDao filmeDao;
-	
+
 	@Autowired
 	private SessaoDao sessaoDao;
 
@@ -40,14 +41,23 @@ public class SessaoController {
 
 		return modelAndView;
 	}
-	
+
 	@PostMapping(value = "/admin/sessao")
 	@Transactional
 	public ModelAndView salva(@Valid SessaoForm form, BindingResult result) {
-		if(result.hasErrors()) return form(form.getSalaId(), form);
+		if (result.hasErrors())
+			return form(form.getSalaId(), form);
 		Sessao sessao = form.toSessao(salaDao, filmeDao);
 		sessaoDao.save(sessao);
-		
+
 		return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
+	}
+
+	@GetMapping("/sessao/{id}/lugares")
+	public ModelAndView lugaresNaSessao(@PathVariable("id") Integer sessaoId) {
+		ModelAndView modelAndView = new ModelAndView("sessao/lugares");
+		Sessao sessao = sessaoDao.findOne(sessaoId);
+		modelAndView.addObject("sessao", sessao);
+		return modelAndView;
 	}
 }
