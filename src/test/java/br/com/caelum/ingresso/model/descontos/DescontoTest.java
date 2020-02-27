c7f86d4d0eebf00903365485f13bea6423954292
Ivan Matuck <ivan.matuck@hotmail.com>
2020-02-26 22:37:50 -0300

    Adicionando catálogo de filmes e puxando detalhes de um cliente

--- src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
@@ -16,7 +16,7 @@ public class DescontoTest {
 	@Test
 	public void naoDeveConcederDescontoParaIngressoNormal() {
 		Sala sala = new Sala("Eldorado	-	IMAX", new BigDecimal("20.5"));
-		Filme filme = new Filme("Rogue	One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
+		Filme filme = new Filme("Rogue	One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12.0"));
 		
 		Sessao sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
 		Ingresso ingresso = new Ingresso(sessao, new SemDesconto());
